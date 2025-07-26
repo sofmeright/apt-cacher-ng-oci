599a474010335c968581e4790618aaf26b5c378e
SoFMeRight <kai.hamil@gmail.com>
2025-07-26 14:12:03 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /scripts/release-notes.sh

---
+++ scripts/release-notes.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+RELEASE=${GIT_TAG:-$1}
+
+if [ -z "${RELEASE}" ]; then
+  echo "Usage:"
+  echo "./scripts/release-notes.sh v0.1.0"
+  exit 1
+fi
+
+if ! git rev-list "${RELEASE}" >/dev/null 2>&1; then
+  echo "Tag ${RELEASE} does not exist"
+  exit 1
+fi
+
+PREV_RELEASE=$(git describe --tags --abbrev=0 "${RELEASE}^" 2>/dev/null) || \
+PREV_RELEASE=$(git rev-list --max-parents=0 "${RELEASE}^")
+
+NOTABLE_CHANGES=$(git tag -l --format='%(contents)' "$RELEASE" | sed '/-----BEGIN PGP SIGNATURE-----/,//d' | tail -n +6)
+CHANGELOG=$(git log --no-merges --pretty=format:'- [%h] %s (%aN)' "${PREV_RELEASE}..${RELEASE}")
+
+cat <<EOF
+${NOTABLE_CHANGES}
+
+## Docker Images for prplanit/apt-cacher-ng:${RELEASE}
+
+- [docker.io](https://hub.docker.com/r/prplanit/apt-cacher-ng/tags)
+
+## Installation
+
+For installation and usage instructions, please refer to the [README](https://gitlab.prplanit.com/yourgroup/yourproject/-/blob/${RELEASE}/README.md)
+
+## Contributing
+
+If you find this image useful, you can help:
+
+- Submit a Merge Request with new features or fixes
+- Report bugs or issues on [GitLab Issues](https://gitlab.prplanit.com/yourgroup/yourproject/-/issues)
+
+## Changelog
+
+${CHANGELOG}
+EOF
